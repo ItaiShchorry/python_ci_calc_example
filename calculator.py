67a6e45d24320c2e26f0e5b17bd69bf39b3dd83c
ItaiShchorry <itaishch@gmail.com>
2018-12-01 19:05:23 +0200

    first functions

---
+++ calculator.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec  1 19:03:10 2018
+
+@author: ishchorry
+"""
+
+"""
+Calculator library containing basic math operations.
+"""
+
+def add(first_term, second_term):
+    return first_term + second_term
+
+def subtract(first_term, second_term):
+    return first_term - second_term
